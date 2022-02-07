13d2bef0c6607569082fd5f39bd7666a1d24f5d2
Tamra Peterson <peterson.tamra@gmail.com>
2022-02-07 11:28:59 -0700

    scoreboard

---
+++ app.js
@@ -0,0 +1,16 @@
+let scoreboardOne = 0
+let scoreboardTwo = 0
+
+function scoreOne() {
+  scoreboardOne++
+  console.log(scoreboardOne);
+}
+
+function scoreTwo() {
+  scoreboardTwo++
+  console.log(scoreboardTwo)
+}
+
+function drawScore() {
+  let scoreElement = document.getElementById("score")
+}
