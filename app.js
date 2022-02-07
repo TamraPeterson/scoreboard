6c8a402dd5f0494fb3f7a9dba2319a1c1dae5ce7
Tamra Peterson <peterson.tamra@gmail.com>
2022-02-07 16:52:37 -0700

    2/7/22

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 let scoreboardOne = 0
 let scoreboardTwo = 0
 
-function scoreOne() {
-  scoreboardOne++
-  console.log(scoreboardOne);
-}
 
-function scoreTwo() {
-  scoreboardTwo++
-  console.log(scoreboardTwo)
-}
 
-function drawScore() {
-  let scoreElement = document.getElementById("score")
+function increaseScore(team) {
+  console.log(team);
+  if (team == "team1") {
+    scoreboardOne++
+    let score1Element = document.getElementById("team1score")
+    score1Element.innerText = scoreboardOne
+  }
+  else {
+    scoreboardTwo++
+    let score2Element = document.getElementById("team2score")
+    score2Element.innerText = scoreboardTwo
+  }
+
+}
+function resetScore() {
+  console.log("reset")
+  scoreboardOne = 0
+  scoreboardTwo = 0
+  document.getElementById('team1score').innerText = 0
+  document.getElementById('team2score').innerText = 0
+
+
 }
